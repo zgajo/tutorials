5634969fa0c45525271709fdc53654a86c8ae961
Darko Pranjic <dpranjic@iolap.com>
2020-01-07 16:20:09 +0100

    set link to services into env variables

--- microservices-app-with-docker-and-graphql/api-gateway/src/helpers/constants.ts
+++ microservices-app-with-docker-and-graphql/api-gateway/src/helpers/constants.ts
@@ -1,8 +1,8 @@
-export const LISTINGS_SERVICE_URL = "http://listings-service";
-export const LISTINGS_SERVICE_PORT = 7100;
+export const LISTINGS_SERVICE_URL = process.env.LISTINGS_SERVICE_URI;
+export const LISTINGS_SERVICE_PORT = process.env.LISTINGS_SERVICE_PORT;
 export const LISTINGS_SERVICE_URI =
   LISTINGS_SERVICE_URL + ":" + LISTINGS_SERVICE_PORT;
 
-export const USERS_SERVICE_URL = "http://users-service";
-export const USERS_SERVICE_PORT = 7101;
+export const USERS_SERVICE_URL = process.env.USERS_SERVICE_URI;
+export const USERS_SERVICE_PORT = process.env.USERS_SERVICE_PORT;
 export const USERS_SERVICE_URI = USERS_SERVICE_URL + ":" + USERS_SERVICE_PORT;
